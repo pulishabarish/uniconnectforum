de886e0ad860cf4d62833f49719d21078b503d67
pulishabarish <125951623+pulishabarish@users.noreply.github.com>
2026-04-20 22:38:38 -0400

    Add backend root health check

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import health_check
 
 
 urlpatterns = [
+    path("", health_check, name="health-check"),
+    path("health/", health_check, name="health-check"),
     path("admin/", admin.site.urls),
     path('api/', include('api.urls')),
 ]
